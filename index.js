d48035368a4ba070fceb3c77a8b2a24c1f744157
sravanthikamani <138889018+sravanthikamani@users.noreply.github.com>
2024-05-23 17:42:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
+/*Here Button component is created by sending the props*/
 const Button = props => {
-  const {buttonType, className} = props
+  const {buttonType, className} = props //destructuring the props
   return <button className={`buttonType,${className}`}>{buttonType}</button>
 }
 
@@ -7,7 +8,7 @@ const element = (
   <div className='bg-container'>
     <h1 className='heading'>Social Buttons</h1>
     <div className='buttons-class'>
-      <Button className='like-button' buttonType='Like' />
+      <Button className='like-button' buttonType='Like' /> //component is used with props
       <Button className='comment-button' buttonType='Comment' />
       <Button className='share-button' buttonType='Share' />
     </div>
